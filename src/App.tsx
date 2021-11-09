83f9470a78e2faa9ce43490ecb0038625f8e341c
LinhTranTD <linhtran.td@one-line.com>
2021-11-09 14:24:28 +0700

    Create left-sidebar in the home page contain the list category

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import Button from './components/Button/Button';
+import LeftSideBar from './components/LeftSideBar/left-side-bar';
 import './App.css';
 
 function App() {
