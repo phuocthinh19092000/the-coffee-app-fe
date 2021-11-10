4e2853ff0278706d8873e075474847881cfd6cff
Ngo Thanh Nhan <nhan.ngot@one-line.com>
2021-11-10 23:29:13 +0700

    feat: create category model

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,17 @@ import './App.scss';
 import LeftSideBar from './components/LeftSideBar/left-side-bar';
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <>
-      <LeftSideBar />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
