7e5ae2885b298dba3a6b814252c88ea2b5975295
Ngo Thanh Nhan <nhan.ngot@one-line.com>
2021-11-10 23:28:30 +0700

    feat: create category model

--- src/components/LeftSideBar/left-side-bar.tsx
+++ src/components/LeftSideBar/left-side-bar.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import '../LeftSideBar/left-side-bar.scss';
+import CategoryItem from '../CategoryItem/category-item';
 
-type Props = {
-  titleItem: string;
+// type Props = {
+//   titleItem: string;
+// };
+// const LeftSideBar = (props: Props) => {
+//   return <button className="item margin-top">{props.titleItem}</button>;
+// };
+
+export type CategoryType = {
+  name: string;
 };
-const LeftSideBar = (props: Props) => {
-  return <button className="item margin-top">{props.titleItem}</button>;
-};
+
+function LeftSideBar() {
+  const [itemData, setItemData] = useState([] as CategoryType[]);
+  useEffect(() => {
+    axios
+      .get('./seed_product_types.json')
+      .then((res) => setItemData(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div className="container">
+      {itemData &&
+        itemData.map((item, index) => (
+          <div key={index} className="item">
+            <CategoryItem item={item} />
+          </div>
+        ))}
+    </div>
+  );
+}
 
 export default LeftSideBar;
