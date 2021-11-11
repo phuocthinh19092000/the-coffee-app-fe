5fb00f7737f1bb2ee6188c5cd830ba92fc4420e4
Ngo Thanh Nhan <nhan.ngot@one-line.com>
2021-11-11 15:03:50 +0700

    fix: using import file json instead of using axios

---
+++ src/Data/seed_products.js
@@ -0,0 +1,12 @@
+export const DrinkData = [
+  { id: 1, name: 'Mango yogurt', price: 19000, type: 'yogurt', image: './images/blackcoffee.png' },
+  { id: 2, name: 'Ice Tea', price: 21000, type: 'tea', image: './images/blackcoffee.png' },
+  { id: 3, name: 'Oreo Ice Blended', price: 35000, type: 'ice blended', image: './images/blackcoffee.png' },
+  { id: 4, name: 'Oreo Ice Blended1', price: 35000, type: 'ice blended', image: './images/blackcoffee.png' },
+  { id: 5, name: 'Oreo Ice Blended2', price: 35000, type: 'ice blended', image: './images/blackcoffee.png' },
+  { id: 6, name: 'Oreo Ice Blended3', price: 35000, type: 'ice blended', image: './images/blackcoffee.png' },
+  { id: 7, name: 'Oreo Ice Blended4', price: 35000, type: 'ice blended', image: './images/blackcoffee.png' },
+  { id: 8, name: 'Oreo Ice Blended5', price: 35000, type: 'ice blended', image: './images/blackcoffee.png' },
+  { id: 9, name: 'Oreo Ice Blended6', price: 35000, type: 'ice blended', image: './images/blackcoffee.png' },
+  { id: 10, name: 'Oreo Ice Blended7', price: 35000, type: 'ice blended', image: './images/blackcoffee.png' },
+];
--- src/components/ListDrinkItem/list-drink-item.tsx
+++ src/components/ListDrinkItem/list-drink-item.tsx
@@ -1,22 +1,19 @@
-import { useState, useEffect } from 'react';
 import DrinkItem from '../DrinkItem/drink-item';
 import './list-drink-item.scss';
-
+import { DrinkData } from '../../Data/seed_products';
 export type DrinkItemType = {
   id: number;
-  category: string;
+  type: string;
   name: string;
   image: string;
   price: number;
 };
 
 function ListDrinkItem() {
-  const [itemData, setItemData] = useState([] as DrinkItemType[]);
-
   return (
     <div className="container">
-      {itemData &&
-        itemData.map((item) => (
+      {DrinkData &&
+        DrinkData.map((item) => (
           <div key={item.id}>
             <DrinkItem item={item} />
           </div>
