7fcb1179851c70c647796057702be70157fe4330
chinh-duongv-otsv <chinh.duongv@one-line.com>
2021-11-12 15:11:55 +0700

    fixbug: clost outside pop up drink detail

--- src/components/ListDrinkItem/list-drink-item.tsx
+++ src/components/ListDrinkItem/list-drink-item.tsx
@@ -1,8 +1,8 @@
 import DrinkItem from '../DrinkItem/drink-item';
 import './list-drink-item.scss';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import DrinkItemDetail from '../DrinkDetail/DrinkItemDetail';
-import ListProduct from '../../json/seed_products.json'
+import ListProduct from '../../json/seed_products.json';
 
 type DrinkItem = {
   id: number;
@@ -14,19 +14,33 @@ type DrinkItem = {
 
 function ListDrinkItem() {
   const [isOpen, setIsOpen] = useState(false);
-  const [itemDrink, setItemDrink] = useState({} as DrinkItem)
-
+  const [itemDrink, setItemDrink] = useState({} as DrinkItem);
   const togglePopup = (item: DrinkItem) => {
-    setIsOpen(!isOpen)
-    setItemDrink(item)
+    setIsOpen(!isOpen);
+    setItemDrink(item);
   };
 
+  const popUpDrinkItemRef = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = (event: Event) => {
+    if (popUpDrinkItemRef.current && !popUpDrinkItemRef.current.contains(event.target as Node)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside, true);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true);
+    };
+  }, [isOpen]);
+
   return (
-    <div className="container">
-      {ListProduct &&
-        ListProduct.map((item) => (
-          <DrinkItem item={item} key={item.id} onClick={() => togglePopup(item)} />
-        ))}
+    <div className="container" ref={popUpDrinkItemRef}>
+      {ListProduct.map((item) => (
+        <DrinkItem item={item} key={item.id} onClick={() => togglePopup(item)} />
+      ))}
       {isOpen && <DrinkItemDetail item={itemDrink} onClick={() => togglePopup(itemDrink)} />}
     </div>
   );
