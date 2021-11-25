b0a92422590416048da300183717ad38a5c021cd
Nhan Ngo T <nhan.ngot@one-line.com>
2021-11-25 22:09:32 +0700

    feat: separated router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import HomePage from './pages/HomePage/HomePage';
 import ErrorPage from './pages/ErrorPage/ErrorPage';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import WrapperPage from './components/WrapperPage/WrapperPage';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import LoginForm from './components/Login/LoginForm';
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
-        <Route path="/wrapper">
-          <WrapperPage />
-        </Route>
-        <Route>
-          <ErrorPage />
-        </Route>
+        <Route exact path="/" component={HomePage} />
+        <Route
+          exact
+          path="/login"
+          render={() => {
+            return !!localStorage.getItem('user') ? <Redirect to="/" /> : <LoginForm />;
+          }}
+        />
+        <Route
+          exact
+          path="/homepage"
+          render={() => {
+            return !!localStorage.getItem('user') ? <HomePage /> : <Redirect to="/login" />;
+          }}
+        />
+        <Route component={ErrorPage} />
       </Switch>
     </Router>
   );
