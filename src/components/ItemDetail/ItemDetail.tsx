5f4f58bb39a00e20754d10e9b2d527ecddd46975
Nhan Ngo T <nhan.ngot@one-line.com>
2021-11-29 15:56:12 +0700

    fix: fix money format in search item

--- src/components/ItemDetail/ItemDetail.tsx
+++ src/components/ItemDetail/ItemDetail.tsx
@@ -1,14 +1,19 @@
 import './ItemDetail.scss';
+import { moneyPattern } from '../../utils/regex';
+
 type Props = {
   className?: string;
   name?: string;
   price?: string;
 };
 const ItemDetail = (props: Props) => {
+  const moneyFormat = (price: number) => {
+    return price.toString().replace(moneyPattern, ',');
+  };
   return (
     <div className={props.className}>
-      <p className="search-detail__name">{props.name || 'Coffee'}</p>
-      <p className="search-detail__price">{props.price || '20.000'}</p>
+      <p className="search-detail__name">{props.name}</p>
+      <p className="search-detail__price">{moneyFormat(Number(props.price))}đ</p>
     </div>
   );
 };
