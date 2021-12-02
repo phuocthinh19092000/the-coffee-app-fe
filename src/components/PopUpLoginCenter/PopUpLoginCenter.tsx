adb020f85d42284926444054406ee8eec469a1f1
Nhan Ngo T <nhan.ngot@one-line.com>
2021-12-02 16:06:30 +0700

    feat: create dialog login center

---
+++ src/components/PopUpLoginCenter/PopUpLoginCenter.tsx
@@ -0,0 +1,26 @@
+import Card from '../Card/Index';
+import Button from '../Button/Index';
+import Exit from '../../share/assets/vector/Exit.svg';
+import './PopUpLoginCenter.scss';
+import FormLogin from '../FormLogin/FormLogin';
+type Props = {
+  onClick?: React.MouseEventHandler<HTMLElement>;
+};
+
+function PopUpLoginCenter(props: Props) {
+  return (
+    <div className="card-login-center--blur">
+      <Card className="card card--center card-login-center">
+        <a>
+          <img src={Exit} className="card-login-center__exit" onClick={props.onClick}></img>
+        </a>
+        <p className="card-login-center__description">Please login to proceed the order:</p>
+        <div>
+          <FormLogin />
+        </div>
+      </Card>
+    </div>
+  );
+}
+
+export default PopUpLoginCenter;
---
+++ src/components/PopUpLoginRight/PopUpLoginRight.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import Card from '../../components/Card/Index';
+import Button from '../../components/Button/Index';
+import Input from '../../components/Input/Input';
+import Icon from '../../components/Icon/Icon';
+import LoginIcon from '../../share/assets/img/Login.png';
+import FacebookIcon from '../../share/assets/vector/VectorFacebook.svg';
+import InstaIcon from '../../share/assets/vector/VectorInsta.svg';
+import LinkedinIcon from '../../share/assets/vector/VectorLinkedin.svg';
+import './PopUpLoginRight.scss';
+import FormLogin from '../../components/FormLogin/FormLogin';
+
+const PopUpLoginRight = () => {
+  return (
+    <Card className={`card card-login card--right`}>
+      <div>
+        <img src={LoginIcon} alt="" />
+        <div>
+          <FormLogin />
+        </div>
+      </div>
+      <div>
+        <div className="brand-media">
+          <Icon href="https://www.facebook.com/OneTechStopVietnam/" src={FacebookIcon} className="icon"></Icon>
+          <Icon href="https://www.instagram.com/OneTechStopVietnam/" src={InstaIcon} className="icon"></Icon>
+          <Icon href="https://www.linkedin.com/OneTechStopVietnam/" src={LinkedinIcon} className="icon"></Icon>
+        </div>
+        <div className="card__content">
+          <p>ONE TECH STOP VIET NAM</p>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default PopUpLoginRight;
