3440ae5417143c5c7e89fd3220bcd4ac8b54ccdb
nhan-ngo-otsv <nhan.ngot@one-line.com>
2022-01-12 17:03:57 +0700

    feat: create login page for staff

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,24 +2,25 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ErrorPage from '../pages/404Page/ErrorPage';
 import ComingSoonPage from '../pages/ComingSoonPage/ComingSoonPage';
 import HomePage from '../pages/HomePage/HomePage';
+import LoginStaff from '../pages/Login-Staff/LoginStaff';
 
 const RouterPage = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={HomePage} />
-
+        <Route exact path='/' component={HomePage} />
+        
         <Route
           exact
-          path="/homepage"
+          path='/homepage'
           render={() => {
             return !!localStorage.getItem('user') ? <HomePage /> : <ComingSoonPage />;
           }}
         />
-
-        <Route exact path="/user/changeAvatar" component={ComingSoonPage} />
-        <Route exact path="/user/changePassword" component={ComingSoonPage} />
-
+        
+        <Route exact path='/staff-login' component={LoginStaff} />
+        <Route exact path='/user/changePassword' component={ComingSoonPage} />
+        
         <Route component={ErrorPage} />
       </Switch>
     </Router>
