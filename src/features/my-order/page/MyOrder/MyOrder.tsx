4e2e6a8964d9d602f239fe311e6b5681354c4ce8
Thinh Tran P <thinh.tranp@one-line.com>
2022-01-13 14:08:34 +0700

    fix: state loading component my order

--- src/features/my-order/page/MyOrder/MyOrder.tsx
+++ src/features/my-order/page/MyOrder/MyOrder.tsx
@@ -1,40 +1,51 @@
 import Card from '../../../../components/Card/Index';
 import OrderItem from '../../components/OrderItem/OrderItem';
 import './MyOrder.scss';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { getUserDataState } from '../../../auth/actions/getUserInfo';
 import Empty from '../../../../share/assets/img/Empty.png';
-import Order from '../../../../interfaces/order';
 import { groupBy } from 'lodash';
 import { datePattern } from '../../../../utils/dateRegex';
 import dayjs from 'dayjs';
 import OrderDate from '../../components/OrderDate/OrderDate';
+import { useAppDispatch } from '../../../../storage/hooks';
+import { getMyOrderLoading, getMyOrders, getMyOrderState } from '../../actions/historyOrder';
+import Spinner from '../../../../components/Spinner/Spinner';
 type Props = {
   onClick?: React.MouseEventHandler<HTMLElement>;
-  listOrder: Order[];
 };
 const MyOrder = (props: Props) => {
   const { freeUnit } = useSelector(getUserDataState);
-  let data = groupBy(props.listOrder, function (date) {
+  const dispatch = useAppDispatch();
+  const myOrder = useSelector(getMyOrderState);
+  const loading = useSelector(getMyOrderLoading);
+
+  useEffect(() => {
+    dispatch(getMyOrders()).unwrap();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  let orderData = groupBy(myOrder, function (date) {
     return dayjs(date.createdAt).format(datePattern);
   });
-  const orderData = Object.entries(data);
   return (
     <div className="my-order--blur">
       <div className="out-side" onClick={props.onClick}></div>
       <Card className={`card card-login card--right my-order`}>
-        {orderData.length > 0 ? (
+        {loading === 'pending' ? (
+          <Spinner />
+        ) : Object.keys(orderData).length > 0 ? (
           <>
             <span className="my-order__title">My Orders</span>
             <span className="my-order__freeUnit">
               Today Free Units: <span className="accent">{freeUnit} </span>
             </span>
             <div className="my-order__list-order">
-              {orderData.map((item) => (
-                <div className="my-order__list-order" key={item[0]}>
-                  <OrderDate date={item[0]} />
-                  {item[1].map((order) => (
+              {Object.keys(orderData).map((date) => (
+                <div className="my-order__list-order" key={date}>
+                  <OrderDate date={date} />
+                  {orderData[date].map((order) => (
                     <OrderItem item={order} key={order.id} />
                   ))}
                 </div>
