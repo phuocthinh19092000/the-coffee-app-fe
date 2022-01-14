2539e07c88412fde6550350462a21695f0a1db67
nhan-ngo-otsv <nhan.ngot@one-line.com>
2022-01-14 13:52:15 +0700

    fix: list product view

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -2,6 +2,7 @@ import Button from '../Button/Index';
 import { useHistory } from 'react-router-dom';
 import Imgage from '../../share/assets/img/error.png';
 import './Error.scss';
+
 const Error = () => {
   const history = useHistory();
   const goHome = () => {
@@ -9,12 +10,14 @@ const Error = () => {
     history.push(path);
   };
   return (
-    <div className="error-page">
-      <img src={Imgage} alt="Error" className="error-page__img" />
-      <h2 className="error-page__title">404 - Page Not Found!</h2>
+    <div className='error-page'>
+      <div className='w-[500px] h-[500px] flex justify-center items-center'>
+        <img src={Imgage} alt='Error' />
+      </div>
+      <h2 className='error-page__title'>404 - Page Not Found!</h2>
       <Button
-        className="btn btn-primary btn--enabled error-page__button "
-        titleButton="BACK TO HOME"
+        className='btn btn-primary btn--enabled error-page__button '
+        titleButton='BACK TO HOME'
         onClick={goHome}
       />
     </div>
--- src/features/product/components/DrinkItem/DrinkItem.tsx
+++ src/features/product/components/DrinkItem/DrinkItem.tsx
@@ -3,6 +3,7 @@ import { moneyPattern } from '../../../../utils/moneyRegex';
 import Product from '../../../../interfaces/product';
 import CoffeeImg from '../../../../share/assets/img/blackcoffee.png';
 import React from 'react';
+
 type Props = {
   item: Product;
   onClick?: React.MouseEventHandler<HTMLDivElement>;
@@ -13,20 +14,21 @@ function DrinkItem(props: Props) {
     return price.toString().replace(moneyPattern, ',');
   };
   return (
-    <div className="drink-item" onClick={props.onClick}>
-      <div className="drink-item__image">
+    <div className='drink-item' onClick={props.onClick}>
+      <div className='drink-item__image'>
+        {/*// TODO: get image from API*/}
         {/* <img src={props.item.avatarUrl} alt={props.item.name} /> */}
         <img src={CoffeeImg} alt={props.item.name} />
       </div>
       <div>
         <div>
-          <span className="drink-item__name">
+          <span className='drink-item__name'>
             {props.item.name}
             <br />
           </span>
         </div>
         <div>
-          <span className="drink-item__price">{moneyFormat(props.item.price)}đ</span>
+          <span className='drink-item__price'>{moneyFormat(props.item.price)}đ</span>
         </div>
       </div>
     </div>
