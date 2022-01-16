552b9fe91d15ebdcb180f748220a7b7c523631b3
chinh-duongv-otsv <chinh.duongv@one-line.com>
2022-01-16 23:20:51 +0700

    feat: create component toast notification and handle logic login page

--- src/features/staff/components/FormLoginStaff/FormLoginStaff.tsx
+++ src/features/staff/components/FormLoginStaff/FormLoginStaff.tsx
@@ -4,10 +4,13 @@ import * as yup from 'yup';
 import './FormLoginStaff.scss';
 import { useAppDispatch } from '../../../../storage/hooks';
 import { login } from '../../../auth/actions/auth';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import EyeIcon from '../../../../share/assets/vector/Eye.svg';
 import CloseEyeIcon from '../../../../share/assets/img/close-eye.png';
 import { getDeviceToken } from '../../../../services/firebase';
+import { NotificationType } from '../../../../enum/NotificationType';
+import ToastNotification from '../../../../components/ToastNotification/ToatstNotification';
+import { PositionToast } from '../../../../enum/PositionToast';
 
 interface IFormInputs {
   username: string;
@@ -18,7 +21,7 @@ const schema = yup
   .object()
   .shape({
     username: yup.string().min(6, 'Username must be at least 6 characters').required('Username is required'),
-    
+
     password: yup.string().min(8, 'Password must be at least 6 characters').required('Password is required'),
   })
   .required();
@@ -32,17 +35,20 @@ export default function FormLoginStaff() {
   } = useForm<IFormInputs>({
     resolver: yupResolver(schema),
     mode: 'onChange',
-    
+
     defaultValues: {
       username: '',
       password: '',
     },
   });
+
   const [loginFailed, setLoginFailed] = useState(false);
+  const timeOutToast = 3000;
   const [showPassword, setShowPassword] = useState(false);
   const toggleShowPassword = () => {
     setShowPassword(!showPassword);
   };
+
   const dispatch = useAppDispatch();
   const onSubmit: SubmitHandler<IFormInputs> = async (data) => {
     const deviceToken = await getDeviceToken();
@@ -56,28 +62,50 @@ export default function FormLoginStaff() {
       reset({ username: '', password: '' });
     }
   };
-  
+
+  useEffect(() => {
+    if (loginFailed) {
+      const timer = setTimeout(() => {
+        setLoginFailed(false);
+      }, timeOutToast);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [loginFailed]);
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div className='input-field-staff'>
-        <input {...register('username')} placeholder='Username' className='staff-input' />
+      <div className="input-field-staff">
+        <input {...register('username')} placeholder="Username" className="staff-input" />
       </div>
-      {errors.username && <p className='error'>{errors.username?.message}</p>}
-      
-      <div className='input-field-staff'>
-        <input type={showPassword ? 'text' : 'password'} {...register('password')} placeholder='Password'
-               className='staff-input' />
+      {errors.username && <p className="error">{errors.username?.message}</p>}
+
+      <div className="input-field-staff">
+        <input
+          type={showPassword ? 'text' : 'password'}
+          {...register('password')}
+          placeholder="Password"
+          className="staff-input"
+        />
         <img
           src={showPassword ? CloseEyeIcon : EyeIcon}
           onClick={toggleShowPassword}
-          className='cursor-pointer absolute right-4 top-3'
-          alt='Icon Password'
+          className="cursor-pointer absolute right-4 top-3"
+          alt="Icon Password"
         />
       </div>
-      {errors.password && <p className='error'>{errors.password?.message}</p>}
-      
-      {loginFailed && <p className='error'>{'Username or password incorrect'}</p>}
-      <button className='staff-login-btn' type='submit'>
+      {errors.password && <p className="error">{errors.password?.message}</p>}
+
+      {loginFailed && (
+        <ToastNotification
+          type={NotificationType.FAILURE}
+          message="Username or password incorrect"
+          position={PositionToast.TOP_CENTER}
+        />
+      )}
+      <button className="staff-login-btn" type="submit">
         LOGIN
       </button>
     </form>
