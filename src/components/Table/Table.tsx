1d16d9e18329f3fbe383210b267540aebaa6b65e
nhan-ngo-otsv <nhan.ngot@one-line.com>
2022-02-23 12:49:13 +0700

    feat: add component empty item for staff

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -4,6 +4,7 @@ import { moneyFormat } from '../../utils/MoneyFormat';
 import Dropdown from '../Dropdown/Dropdown';
 import BlackCoffee from '../../share/assets/img/blackcoffee.png';
 import './Table.scss';
+import ProductCart from '../../share/assets/vector/ProductCart.svg';
 
 type Props = {
   header: Array<string | JSX.Element>;
@@ -17,67 +18,78 @@ const Table = (props: Props) => {
   const body = props.body;
 
   return (
-    <table className="table">
-      <thead className="table-header">
-        <tr>
-          {header.map((header, index) => (
-            <th key={index}>{header}</th>
-          ))}
-        </tr>
-      </thead>
+    <>
+      <table className="table">
+        <thead className="table-header">
+          <tr>
+            {header.map((header, index) => (
+              <th key={index}>{header}</th>
+            ))}
+          </tr>
+        </thead>
 
-      <tbody>
-        {body &&
-          body.map((object, index) => (
-            <tr key={index} className="table-body__row">
-              {props.startIndex && (
-                <td key={object.id} className="table-body__cell">
-                  {props.startIndex + index}
-                </td>
-              )}
+        {body.length > 0 && (
+          <tbody>
+            {body.map((object, index) => (
+              <tr key={index} className="table-body__row">
+                {props.startIndex && (
+                  <td key={object.id} className="table-body__cell">
+                    {props.startIndex + index}
+                  </td>
+                )}
 
-              {Object.entries(object).map((obj, index) => {
-                const key = obj[0];
-                let value = obj[1];
+                {Object.entries(object).map((obj, index) => {
+                  const key = obj[0];
+                  let value = obj[1];
 
-                if (typeof value === 'number') {
-                  value = moneyFormat(value);
-                }
+                  if (typeof value === 'number') {
+                    value = moneyFormat(value);
+                  }
 
-                if (key === 'id') {
-                  // eslint-disable-next-line array-callback-return
-                  return;
-                } else if (key === 'images') {
+                  if (key === 'id') {
+                    // eslint-disable-next-line array-callback-return
+                    return;
+                  } else if (key === 'images') {
+                    return (
+                      <td key={index}>
+                        <img className="table-body__img" src={value || BlackCoffee} alt="Avatar Drink" />
+                      </td>
+                    );
+                  }
+
                   return (
-                    <td key={index}>
-                      <img className="table-body__img" src={value || BlackCoffee} alt="Avatar Drink" />
+                    <td
+                      key={index}
+                      className={`table-body__cell ${
+                        value === ProductStatus.OUT_OF_STOCK || value === UserStatus.IN_ACTIVE
+                          ? 'table-body__cell--accent'
+                          : ''
+                      }`}
+                    >
+                      {value}
                     </td>
                   );
-                }
+                })}
 
-                return (
-                  <td
-                    key={index}
-                    className={`table-body__cell ${
-                      value === ProductStatus.OUT_OF_STOCK || value === UserStatus.IN_ACTIVE
-                        ? 'table-body__cell--accent'
-                        : ''
-                    }`}
-                  >
-                    {value}
+                {props.isHaveDropdown && (
+                  <td className="table-body__cell grid justify-items-center">
+                    <Dropdown selectedValue={object} />
                   </td>
-                );
-              })}
-
-              {props.isHaveDropdown && (
-                <td className="table-body__cell grid justify-items-center">
-                  <Dropdown selectedValue={object} />
-                </td>
-              )}
-            </tr>
-          ))}
-      </tbody>
-    </table>
+                )}
+              </tr>
+            ))}
+          </tbody>
+        )}
+      </table>
+      {body.length === 0 && (
+        <div className="table-body__empty">
+          <div className="table-body__group">
+            <img src={ProductCart} alt="Product Cart" />
+            <p className="pt-2">No item is added yet.</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
