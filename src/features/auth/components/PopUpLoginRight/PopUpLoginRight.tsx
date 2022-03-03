c47d849801c9a9f56ab3ea8c5b392bf15dc1abd6
nhan-ngo-otsv <nhan.ngot@one-line.com>
2022-03-04 01:40:28 +0700

    fix: fix layout component login right

--- src/features/auth/components/PopUpLoginRight/PopUpLoginRight.tsx
+++ src/features/auth/components/PopUpLoginRight/PopUpLoginRight.tsx
@@ -4,22 +4,22 @@ import LoginIcon from '../../../../share/assets/img/Login.png';
 import FacebookIcon from '../../../../share/assets/vector/VectorFacebook.svg';
 import InstaIcon from '../../../../share/assets/vector/VectorInsta.svg';
 import LinkedinIcon from '../../../../share/assets/vector/VectorLinkedin.svg';
-import './PopUpLoginRight.scss';
+import './PopUpLoginRight.css';
 import HookForm from '../../../../components/HookForm/HookForm';
 
 const PopUpLoginRight = () => {
 	return (
-		<Card className={`card card-login card--right`}>
+		<Card className={`card card-login card--right scroll-popup-right`}>
 			<div>
-				<img src={LoginIcon} alt='' />
+				<img src={LoginIcon} alt=''/>
 				<div>
-					<HookForm />
+					<HookForm/>
 				</div>
 			</div>
 			<div>
 				<div className='brand-media'>
-					<Icon href='https://www.facebook.com/OneTechStopVietnam/' src={FacebookIcon} className='icon' />
-					<Icon href='https://www.instagram.com/OneTechStopVietnam/' src={InstaIcon} className='icon' />
+					<Icon href='https://www.facebook.com/OneTechStopVietnam/' src={FacebookIcon} className='icon'/>
+					<Icon href='https://www.instagram.com/OneTechStopVietnam/' src={InstaIcon} className='icon'/>
 					<Icon href='https://www.linkedin.com/OneTechStopVietnam/' src={LinkedinIcon} className='icon' />
 				</div>
 				<div className='card__content'>
