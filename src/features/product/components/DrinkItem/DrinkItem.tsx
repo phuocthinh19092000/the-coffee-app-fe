c540cd70d74ae5ed9d4148b0e9f9e3c7f64ac0e3
nguyen-dangn-otsv <nguyen.dangn@one-line.com>
2022-03-08 16:18:04 +0700

    feat: creat popup receive notification when canceled order customer

--- src/features/product/components/DrinkItem/DrinkItem.tsx
+++ src/features/product/components/DrinkItem/DrinkItem.tsx
@@ -1,10 +1,10 @@
 import './DrinkItem.scss';
 import { moneyPattern } from '../../../../utils/moneyRegex';
-import { Product } from '../../../../interfaces';
+import { Product, ProductItem } from '../../../../interfaces';
 import React from 'react';
 
 type Props = {
-  item: Product;
+  item: Product | ProductItem;
   onClick?: React.MouseEventHandler<HTMLDivElement>;
 };
 
@@ -17,7 +17,7 @@ function DrinkItem(props: Props) {
       <img className="w-[160px]" src={props.item.images} alt={props.item.name} />
       <div>
         <div>
-          <span className="drink-item__name">
+          <span className="drink-item__name mt-1.5 text-style-1440-h1">
             {props.item.name}
             <br />
           </span>
