834230925febb3b2924f539ea9e47dc9867faec3
nhan-ngo-otsv <nhan.ngot@one-line.com>
2022-03-11 11:34:24 +0700

    feat: intergrate API add category and update category information

--- src/features/staff/components/FormManageCategory/FormManageCategory.tsx
+++ src/features/staff/components/FormManageCategory/FormManageCategory.tsx
@@ -1,9 +1,14 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CustomInput from '../../../../components/CustomInput/CustomInput';
 import WrapperForm from '../../../../components/WrapperForm/WrapperForm';
-import { InputParams } from '../../../../interfaces';
+import {InputParams} from '../../../../interfaces';
 import { CategoryTypeDto } from '../../../../interfaces/category';
-import { NotificationParams } from '../../../../interfaces/notificationParams';
+import { NotificationParams } from '../../../../interfaces';
+import {useAppDispatch} from "../../../../storage/hooks";
+import {createCategory, updateCategory} from "../../../product/actions/createCategoryData";
+import {statusCodeError} from "../../../../constant";
+import {NotificationType} from "../../../../enum";
+import {startCase} from "lodash";
 
 type Props = {
   selectedCategory?: CategoryTypeDto;
@@ -16,11 +21,24 @@ type Props = {
 const FormManageCategory = (props: Props) => {
   const [isFullFill, setIsFullFill] = useState(false);
   const [category, setCategory] = useState<CategoryTypeDto>(props.selectedCategory || { id: '', name: '' });
+  const dispatch = useAppDispatch();
 
   const handleChange = (inputParams: InputParams) => {
     if (inputParams.event) {
       const { value } = inputParams.event.target;
       setCategory({ ...category, name: value });
+    }
+  };
+
+  const setContentNotification = (message: string, response: any) => {
+    if (statusCodeError.includes(response.payload.status)) {
+      props.setShowNotification({
+        type: NotificationType.FAILURE,
+        message: message,
+      });
+    } else {
+      props.setShowNotification({ message, type: NotificationType.SUCCESS });
+      props.onSave();
     }
   };
 
@@ -32,10 +50,34 @@ const FormManageCategory = (props: Props) => {
     }
   }, [category]);
 
+  const onSaveDataHandler = async () => {
+    if (!isFullFill) {
+      return;
+    }
+  
+    category.name = startCase(category.name.trim());
+    if (category.id) {
+      const response = await dispatch(updateCategory({categoryId: category.id, body: {name: category.name} }));
+      if (updateCategory.fulfilled.match(response)){
+        setContentNotification('Update Category successfully!', response);
+      } else {
+        setContentNotification('Category name already existed', response);
+      }
+    } else {
+      const response = await dispatch(createCategory({ name: category.name }));
+      if (createCategory.fulfilled.match(response)){
+        setContentNotification('New Category added successfully!', response);
+      } else {
+        setContentNotification('Category name already existed', response);
+      }
+    }
+  };
+
   return (
     <WrapperForm
       name={props.formName}
       isFullFill={isFullFill}
+      onClickSave={onSaveDataHandler}
       onClickExit={props.onClickExit}
       onClickCancel={props.onClickExit}
     >
