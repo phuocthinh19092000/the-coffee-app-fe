64bc4a73d7fc60fcbc30bbc326e8944d9667b87d
chinh-duongv-otsv <chinh.duongv@one-line.com>
2022-03-18 00:01:44 +0700

    feat: reuse input in popup change password customer

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FieldError } from 'react-hook-form';
 import './Input.css';
 
 type Props = {
@@ -13,23 +14,27 @@ type Props = {
   onClickSecondIcon?: React.MouseEventHandler<HTMLImageElement>;
   value?: string | number;
   readOnly?: boolean | false;
+  error?: FieldError;
 };
 const Input = (props: Props) => {
   return (
-    <div className={`${props?.className} block-input`}>
-      <input
-        placeholder={props.placeholder}
-        type={props.type}
-        onChange={props.onChange}
-        readOnly={props.readOnly}
-        onFocus={props.onFocus}
-        value={props.value}
-      />
-      {props.src && <img src={props.src} alt={props.src} className="right-1.5" onClick={props.onClickFirstIcon} />}
-      {props.src2 && (
-        <img src={props.src2} alt={props.src2} className="right-[74px]" onClick={props.onClickSecondIcon} />
-      )}
-    </div>
+    <>
+      <div className={`block-input ${props.className} `}>
+        <input
+          placeholder={props.placeholder}
+          type={props.type}
+          onChange={props.onChange}
+          readOnly={props.readOnly}
+          onFocus={props.onFocus}
+          value={props.value}
+        />
+        {props.src && <img src={props.src} alt={props.src} className="right-1.5" onClick={props.onClickFirstIcon} />}
+        {props.src2 && (
+          <img src={props.src2} alt={props.src2} className="right-[74px]" onClick={props.onClickSecondIcon} />
+        )}
+      </div>
+      {props.error && <p className="text-error ml-[20px] my-[10px]">{props.error.message}</p>}
+    </>
   );
 };
 export default Input;
